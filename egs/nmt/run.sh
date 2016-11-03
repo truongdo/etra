c5475e3007f1d54fa4b893eac8959456d01e2c87
Do Quoc Truong <truongdq54@gmail.com>
2016-11-03 21:48:15 +0900

    Default RMSprop optimizer

--- egs/nmt/run.sh
+++ egs/nmt/run.sh
@@ -7,13 +7,12 @@
 #
 
 
-stage=2
-gpu=2
+stage=0
+gpu=0
 data=data/en-vi
 
 . ./path.sh
 if [[ $stage -le 0 ]]; then
-  # TODO Test to make sure all words in vocab are unique
   steps/get_vocab.sh $data/train exp/lang || exit 1
 fi
 
@@ -22,27 +21,34 @@ if [[ $stage -le 1 ]]; then
   do
     steps/convert_data.sh --batch-size 128 exp/lang $data/$x exp/feat/$x || exit 1
   done
-  steps/convert_data.sh --batch-size 1 exp/lang $data/test exp/feat/test || exit 1
   steps/convert_data.sh --batch-size 128 exp/lang $data/test exp/feat/test_batch || exit 1   # this is for speed up evaluation time
+  steps/convert_data.sh --batch-size 1 exp/lang $data/test exp/feat/test || exit 1
 fi
 
 if [[ $stage -le 2 ]]; then
   epoch=25
   emb_size=256
-  lr_init=0.001
-  lr_stop=0.0001
+  lr_init=0.0001
+  lr_stop=0.00001
   lr_scale=2
   hid_size=1024
   drop_ratio=0.0
   depth=3
-  dir=exp/model
+  optimizer="RMSpropGraves"
+  dir=exp/model_RMSpropGraves
 
-  steps/train_nmt.sh --gpu $gpu --stage 7 --epoch $epoch \
+  steps/train_nmt.sh --gpu $gpu --stage 0 --epoch $epoch \
     --emb-size $emb_size --hid-size $hid_size \
     --lr-init $lr_init --lr-scale $lr_scale --lr-stop $lr_stop \
     --depth $depth --drop-ratio $drop_ratio \
+    --optimizer $optimizer \
     --dev-dir exp/feat/dev exp/lang exp/feat/train $dir
-
+  
+  # Note that to speed up the evaluation, we use minibatch for the test set
+  # which requires padding end-of-sentence charater to sentences
+  # in the same batch to make them the same length, this will lead to
+  # higher error rate than normal. To have the actual error, run the
+  # follow command with exp/feat/test
   $cmd $dir/log/test.log forward-nmt --gpu -1 \
         exp/feat/test_batch $dir $dir/test || exit 1
 
