4566c77ada945b26aaa5733b716ea28e4fcadc2a
Do Quoc Truong <truongdq54@gmail.com>
2016-11-04 14:54:24 +0900

    improvement

--- egs/nmt/run.sh
+++ egs/nmt/run.sh
@@ -9,7 +9,7 @@
 
 stage=0
 gpu=0
-pair=vi2en
+pair=en2vi
 data=data/$pair
 
 . ./path.sh
@@ -27,10 +27,10 @@ fi
 if [[ $stage -le 1 ]]; then
   for x in train dev;
   do
-    steps/convert_data.sh --batch-size 128 exp/lang_$pair $data/$x exp/feat/$x || exit 1
+    steps/convert_data.sh --batch-size 128 exp/lang_$pair $data/$x exp/feat_$pair/$x || exit 1
   done
-  steps/convert_data.sh --batch-size 128 exp/lang_$pair $data/test exp/feat/test_batch || exit 1   # this is for speed up evaluation time
-  steps/convert_data.sh --batch-size 1 exp/lang_$pair $data/test exp/feat/test || exit 1
+  steps/convert_data.sh --batch-size 128 exp/lang_$pair $data/test exp/feat_$pair/test_batch || exit 1   # this is for speed up evaluation time
+  steps/convert_data.sh --batch-size 1 exp/lang_$pair $data/test exp/feat_$pair/test || exit 1
 fi
 
 if [[ $stage -le 2 ]]; then
@@ -44,22 +44,22 @@ if [[ $stage -le 2 ]]; then
   depth=3
   prob_len=15
   optimizer="RMSpropGraves"
-  dir=exp/model_${pair}_RMSpropGraves
+  dir=exp/model_${pair}_RMSpropGraves_h512_d2
 
   steps/train_nmt.sh --gpu $gpu --stage 0 --epoch $epoch \
     --emb-size $emb_size --hid-size $hid_size \
     --lr-init $lr_init --lr-scale $lr_scale --lr-stop $lr_stop \
     --depth $depth --drop-ratio $drop_ratio \
     --optimizer $optimizer --prob-len $prob_len\
-    --dev-dir exp/feat/dev exp/lang_$pair exp/feat/train $dir
+    --dev-dir exp/feat_$pair/dev exp/lang_$pair exp/feat_$pair/train $dir
   
   # Note that to speed up the evaluation, we use minibatch for the test set
   # which requires padding end-of-sentence charater to sentences
   # in the same batch to make them the same length, this will lead to
   # higher error rate than normal. To have the actual error, run the
-  # follow command with exp/feat/test, but it will take more time.
+  # follow command with exp/feat_$pair/test, but it will take more time.
   $cmd $dir/log/test.log forward-nmt --gpu -1 \
-        exp/feat/test_batch $dir $dir/test || exit 1
+        exp/feat_$pair/test_batch $dir $dir/test || exit 1
 
-  steps/decode_nmt.sh --gpu -1 exp/lang_$pair exp/feat/test $dir $dir/decode
+  steps/decode_nmt.sh --gpu -1 exp/lang_$pair exp/feat_$pair/test $dir $dir/decode
 fi
