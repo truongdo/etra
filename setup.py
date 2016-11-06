b8070cc1d976c9569089ac00263ed92690e3db1a
Do Quoc Truong <truongdq54@gmail.com>
2016-11-06 19:11:01 +0900

    Update info

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ import os
 if __name__ == "__main__":
     setup(
         name="etra",
+        packages = ['etra'], 
         install_requires=open(os.path.join(
             os.path.dirname(__file__),
             "req.txt"), 'rb').readlines(),
         version="0.1.0",
+        author = 'Truong Do',
+        author_email = 'truongdq54@gmail.com',
+        url = 'https://github.com/truongdo/etra', # use the URL to the github repo
     )
