e33276647df66d1c299d6b2efb27e6f2ab95a426
Do Quoc Truong <truongdq54@gmail.com>
2016-11-06 19:49:42 +0900

    Fix setup script

--- setup.py
+++ setup.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     setup(
         name="etra",
         packages = find_packages(), 
-        install_requires=open(os.path.join(
-            os.path.dirname(__file__),
-            "req.txt"), 'rb').readlines(),
-        version="0.1.0",
+        install_requires=["setuptools", "nose", "coverage", "six",
+            "chainer", "nvidia-ml-py"],
+        version="0.1.2",
         author = 'Truong Do',
         author_email = 'truongdq54@gmail.com',
         url = 'https://github.com/truongdo/etra', # use the URL to the github repo
